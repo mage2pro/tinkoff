847ef3e3633a140dabc86a1effa2f39fe75e937c
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-08-19 08:34:44 +0300

    0.0.3

--- Charge.php
+++ Charge.php
@@ -7,6 +7,24 @@ namespace Dfe\Tinkoff;
  */
 final class Charge extends \Df\PaypalClone\Charge {
 	/**
+	 * 2017-08-19
+	 * @override
+	 * @see \Df\PaypalClone\Charge::k_Amount()
+	 * @used-by \Df\PaypalClone\Charge::p()
+	 * @return string
+	 */
+	protected function k_Amount() {return '';}
+
+	/**
+	 * 2017-08-19
+	 * @override
+	 * @see \Df\PaypalClone\Charge::k_MerchantId()
+	 * @used-by \Df\PaypalClone\Charge::p()
+	 * @return string
+	 */
+	protected function k_MerchantId() {return '';}
+
+	/**
 	 * 2017-04-18
 	 * @override
 	 * @see \Df\PaypalClone\Charge::pCharge()
